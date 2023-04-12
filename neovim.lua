e194d814e62030cd3e47bc79c7160562a1c563b3
Michal <mikh3x4@gmail.com>
2023-04-12 04:21:45 -0700

    buffer completion

--- neovim.lua
+++ neovim.lua
@@ -118,8 +118,10 @@ require('lazy').setup({
     'hrsh7th/nvim-cmp',
     dependencies = { 'hrsh7th/cmp-nvim-lsp',
                      'L3MON4D3/LuaSnip',
+                     'saadparwaiz1/cmp_luasnip',
                      'hrsh7th/cmp-path',
-                     'saadparwaiz1/cmp_luasnip' },
+                     'hrsh7th/cmp-buffer',
+                    },
   },
 
   -- Useful plugin to show you pending keybinds.
@@ -488,6 +490,7 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = 'buffer' },
     { name = 'path', option = {
          trailing_slash = true,
       }},
