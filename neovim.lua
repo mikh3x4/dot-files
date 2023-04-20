8d90d7fc51e9fce44f81bbf4d78160e15be24d11
Michal <mikh3x4@gmail.com>
2023-04-20 04:54:52 -0700

    fix

--- neovim.lua
+++ neovim.lua
@@ -88,10 +88,6 @@ require('lazy').setup({
       }
       vim.cmd.colorscheme 'onedark'
       vim.g.terminal_color_7 = "#50545c"
-      vim.g.terminal_color_255 = "#000000"
-      vim.g.terminal_color_15 = "#000000"
-      vim.g.terminal_color_250 = "#000000"
-      vim.g.terminal_color_251 = "#000000"
     end,
   },
 
